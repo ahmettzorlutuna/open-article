68758b947b26e4a6c7a029b8569bb8be841df938
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2023-01-19 23:48:34 +0300

    User model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const Post = require('./post')
+const Comment = require('./comment')
+const uuid = require('uuid')
+
+class User{
+    constructor(username, posts = [], following = [], followers = [], messages = [], id = uuid.v4()){
+        this.username = username
+        this.posts = posts
+        this.following = following
+        this.followers = followers
+        this.id = id
+        this.messages = messages
+    }
+    createPost(name,content, comment){
+        const post = new Post(name,content,comment)
+        this.posts.push(post)
+        return post
+    }
+    follow(user){
+        this.following.push(user.username)
+        user.followers.push(this.username)
+    }
+    updateArticleByName(postName,name,content,comment){
+        const index = this.posts.findIndex(o => o.name == postName)
+        this.posts[index].name = name
+        this.posts[index].content = content
+    }
+    makeCommentByName(user,postName,name,content){ //Make comment by name yorumu kim yaptı isim bilgisi (yapıldı)
+        const commentt = new Comment(name,content,this.username)
+        for (let i = 0; i < user.posts.length; i++) {
+            const element = user.posts[i];
+            if(element.name == postName){
+                element.comment.push(commentt)
+            }
+        }
+    }
+    updateComment(user,postName,commentName,name,content){
+        const post = user.posts.findIndex(o => o.name == postName)
+        const comments = user.posts[post].comment
+        const comment = comments.findIndex(o => o.commentName == commentName)
+        comments[comment].commentName = name
+        comments[comment].commentContent = content
+    }
+    likePost(post){ 
+    }
+    sendMessage(user){
+
+    }
+    static create({username,posts,following,followers,id}){
+        return new User(username,posts,following,followers,id)
+    }
+}
+
+module.exports = User
