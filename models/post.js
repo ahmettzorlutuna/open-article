b6b12a32cfb6c0dc2ec17593ce42bc36c775269c
Ahmet Zorlutuna <ahmettayyip3439@gmail.com>
2023-01-19 23:48:25 +0300

    Post model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const uuid = require('uuid')
+
+class Post{
+    constructor(name,content,comment = [],id = uuid.v4()){
+        this.name = name
+        this.content = content
+        this.comment = comment
+        this.id = id
+    }
+}
+
+module.exports = Post
