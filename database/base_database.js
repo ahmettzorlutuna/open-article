fadb7eed24b245dfbf0a194461655448e29a5b86
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-08 23:40:46 +0300

    flatted and removeBy method for api

--- database/base_database.js
+++ database/base_database.js
@@ -8,7 +8,7 @@ class BaseDatabase{
     }
     save(objects){
         return new Promise((resolve, reject)=>{
-            fs.writeFile(`${__dirname}/${this.filename}.json`, JSON.stringify(objects, null, 2),(err)=>{
+            fs.writeFile(`${__dirname}/${this.filename}.json`, flatted.stringify(objects, null, 2),(err)=>{
                 if (err) reject(err)
                 resolve()
             })
@@ -19,18 +19,27 @@ class BaseDatabase{
         return new Promise((resolve,reject)=>{
             fs.readFile(`${__dirname}/${this.filename}.json`,'utf-8',(err,file)=>{  
                 if(err) reject(err)
-                const objects = JSON.parse(file)
+                const objects = flatted.parse(file)
                 resolve(objects.map(this.model.create))
             })
         })
     }
     async insert(object){
         const objects = await this.load()
-        objects.push(object)
+        if(!(object instanceof this.model)){ //İf incoming object is not instance of this.model we created a new object of this model and pushed into the objects
+            object = this.model.create(object)
+            objects.push(object)
+        }
         return this.save(objects)
     }
     async remove(index){
         const objects = await this.load()
+        objects.splice(index, 1)
+        return this.save(objects)
+    }
+    async removeBy(property, value){
+        const objects = await this.load()
+        const index = objects.findIndex(o => o[property] == value)
         objects.splice(index, 1)
         return this.save(objects)
     }
