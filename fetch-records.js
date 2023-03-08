8de00151c1b5389511f491bf9d1f2867eebbef57
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-08 23:41:16 +0300

    some changes

--- fetch-records.js
+++ fetch-records.js
@@ -3,14 +3,18 @@ const userDatabase= require('./database');
 const printAllPosts = require('./lib/print-post-history');
 
 const ahmet = new User("Ahmet")
+const mehmet = new User("Mehmet")
+const selami = new User("Selami")
 
-ahmet.createPost("Ahmet'in Makalesi","Deneme Makalesi")
-ahmet.createPost("Ahmet'in yeni makalesi","Deneme makalesi yeni")
+// ahmet.createPost("Ahmet'in Makalesi","Deneme Makalesi")
+// ahmet.createPost("Ahmet'in yeni makalesi","Deneme makalesi yeni")
+// mehmet.createPost("Mehmet'in yeni makalesi","Mehmet makalesi yeni")
+
 
 
 async function main(){
     try {
-        await userDatabase.save([ahmet])
+        await userDatabase.save([ahmet, mehmet, selami])
         const data = await userDatabase.load()
         console.log(data)
     } catch (error) {
