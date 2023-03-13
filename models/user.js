62ef1d0543a071c2950b42df7624167038733ee5
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-13 23:45:33 +0300

    MakeCommentById

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const Post = require('./post')
 const Comment = require('./comment')
 const uuid = require('uuid')
+const user_database = require('../database')
 
 class User{
-    constructor(username, posts = [], following = [], followers = [], messages = [], userId = uuid.v4()){
-        this.username = username
+    constructor(userName, posts = [], following = [], followers = [], messages = [], userId = uuid.v4()){
+        this.userName = userName
         this.posts = posts
         this.following = following
         this.followers = followers
@@ -25,14 +26,19 @@ class User{
         this.posts[index].name = name
         this.posts[index].content = content
     }
-    makeCommentByName(user,postName,name,content){ //Make comment by name yorumu kim yaptı isim bilgisi (yapıldı)
-        const commentt = new Comment(name,content,this.username)
-        for (let i = 0; i < user.posts.length; i++) {
-            const element = user.posts[i];
-            if(element.name == postName){
-                element.comment.push(commentt)
-            }
-        }
+    // makeCommentByName(user,postName,name,content){ //Make comment by name yorumu kim yaptı isim bilgisi (yapıldı)
+    //     const commentt = new Comment(name,content,this.username)
+    //     for (let i = 0; i < user.posts.length; i++) {
+    //         const element = user.posts[i];
+    //         if(element.name == postName){
+    //             element.comment.push(commentt)
+    //         }
+    //     }
+    // }
+    makeCommentById(postId, user, name, content){
+        const comment = new Comment(name, content, this.userName)
+        const post = user.posts.findIndex(o=> o.postId == postId)
+        user.posts[post].comment.push(comment)
     }
     updateComment(user,postName,commentName,name,content){
         const post = user.posts.findIndex(o => o.name == postName)
@@ -40,8 +46,6 @@ class User{
         const comment = comments.findIndex(o => o.commentName == commentName)
         comments[comment].commentName = name
         comments[comment].commentContent = content
-    }
-    likePost(post){ 
     }
     static create({username,posts,following,followers,messages,userId}){
         return new User(username,posts,following,followers,messages,userId)
