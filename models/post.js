aaadce75f81b49548fa38d32ba1eac84810b2b56
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-17 23:36:47 +0300

    like, dislike attributes

--- models/post.js
+++ models/post.js
@@ -1,10 +1,12 @@
 const uuid = require('uuid')
 
 class Post{
-    constructor(name,content,comment = [],id = uuid.v4()){
+    constructor(name,content,comment = [], likes = 0, dislikes = 0 , id = uuid.v4()){
         this.name = name
         this.content = content
         this.comment = comment
+        this.likes = likes
+        this.dislikes = dislikes
         this.postId = id
     }
 }
