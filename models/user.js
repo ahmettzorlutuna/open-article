0d179a1edaf40205ee45f4b0df1dcc2adec69d16
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-17 23:37:35 +0300

    makeCommentById, like, dislike

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
+const uuid = require('uuid')
 const Post = require('./post')
 const Comment = require('./comment')
-const uuid = require('uuid')
-const user_database = require('../database')
 
 class User{
-    constructor(userName, posts = [], following = [], followers = [], messages = [], userId = uuid.v4()){
-        this.userName = userName
+    constructor(username, posts = [], following = [], followers = [], messages = [], userId = uuid.v4()){
+        this.username = username
         this.posts = posts
         this.following = following
         this.followers = followers
@@ -26,27 +25,17 @@ class User{
         this.posts[index].name = name
         this.posts[index].content = content
     }
-    // makeCommentByName(user,postName,name,content){ //Make comment by name yorumu kim yaptı isim bilgisi (yapıldı)
-    //     const commentt = new Comment(name,content,this.username)
-    //     for (let i = 0; i < user.posts.length; i++) {
-    //         const element = user.posts[i];
-    //         if(element.name == postName){
-    //             element.comment.push(commentt)
-    //         }
-    //     }
-    // }
-    makeCommentById(postId, user, name, content){
-        const comment = new Comment(name, content, this.userName)
-        const post = user.posts.findIndex(o=> o.postId == postId)
-        user.posts[post].comment.push(comment)
+    
+    makeCommentById(post, name, content){
+        const comment = new Comment(name, content, this.username)
+        post.comment.push(comment)
     }
-    updateComment(user,postName,commentName,name,content){
-        const post = user.posts.findIndex(o => o.name == postName)
-        const comments = user.posts[post].comment
-        const comment = comments.findIndex(o => o.commentName == commentName)
-        comments[comment].commentName = name
-        comments[comment].commentContent = content
+    likePost(post){
+       post.likes += 1 
     }
+    dislikePost(post){
+        post.dislikes += 1 
+     }
     static create({username,posts,following,followers,messages,userId}){
         return new User(username,posts,following,followers,messages,userId)
     }
