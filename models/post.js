5dc221309b3f74e1ae932158b83ac5b328595123
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-03-30 00:18:37 +0300

    Post Schema

--- models/post.js
+++ models/post.js
@@ -1,14 +1,25 @@
-const uuid = require('uuid')
+const uuid = require("uuid");
+const mongoose = require("mongoose");
 
-class Post{
-    constructor(name,content,comment = [], likes = 0, dislikes = 0 , id = uuid.v4()){
-        this.name = name
-        this.content = content
-        this.comment = comment
-        this.likes = likes
-        this.dislikes = dislikes
-        this.postId = id
-    }
-}
+const PostSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+  comment: [],
+  likes: { type: Number, default: 0},
+  dislikes: { type: Number, default: 0},
+});
 
-module.exports = Post
+module.exports = mongoose.model("Post", PostSchema);
+
+// class Post{
+//     constructor(name,content,comment = [], likes = 0, dislikes = 0 , id = uuid.v4()){
+//         this.name = name
+//         this.content = content
+//         this.comment = comment
+//         this.likes = likes
+//         this.dislikes = dislikes
+//         this.postId = id
+//     }
+// }
+
+// module.exports = Post
