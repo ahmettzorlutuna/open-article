d00051eeb21ee9c3329740eac566c6eeb7909ca2
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-03 00:43:25 +0300

    likes dislikes default value

--- models/post.js
+++ models/post.js
@@ -5,8 +5,8 @@ const PostSchema = new mongoose.Schema({
   name: String,
   content: String,
   comment: [],
-  likes: { type: Number, default: 0},
-  dislikes: { type: Number, default: 0},
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
