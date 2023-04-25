1be92a3b73b6343b54051d144a745c3c034509b0
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-25 23:21:35 +0300

    New user and New post route updated with services

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const user_database = require("../database");
+const {userService, postService, commentService} = require("../services");
+const Post = require("../models/post")
+
 
 //Users
 router.get("/", async (req, res) => {
-  const users = await user_database.load();
+  const users = await userService.load();
   res.render("users", { users });
 });
 
 //User Detail
 router.get("/:id", async (req, res) => {
-  const user = await user_database.find(req.params.id);
+  const user = await userService.find(req.params.id);
   if (!user) return res.status(404).send("Cannot get user");
   //if(!user.followers) return res.status(404).send('Cannot find followings or followers')
   res.render("user", { user });
@@ -18,13 +20,13 @@ router.get("/:id", async (req, res) => {
 
 //Delete User
 router.delete("/:userId", async (req, res) => {
-  const user = user_database.removeById(req.params.userId);
+  const user = userService.removeById(req.params.userId);
   res.send(user);
 });
 
 //New User
 router.post("/", async (req, res) => {
-  await user_database.insert(req.body);
+  await userService.insert(req.body);
   res.send("Ok");
 });
 
@@ -32,17 +34,16 @@ router.post("/", async (req, res) => {
 router.post("/:userId", async (req, res) => {
   const { name, content } = req.body;
   const { userId } = req.params;
-  const user = await user_database.find(userId);
-  const newPost = user.createPost(req.body);
-  await user.save();
+
+  const newPost = await postService.createPost(userId, name, content);
   res.send(newPost);
 });
 
 //Follow
 router.post("/follow/:userId/:userId2", async (req, res) => {
   const { userId, userId2 } = req.params;
-  const user = await user_database.find(userId);
-  const user2 = await user_database.find(userId2);
+  const user = await userService.find(userId);
+  const user2 = await userService.find(userId2);
   user.follow(user2);
   await user.save();
   await user2.save();
@@ -52,39 +53,38 @@ router.post("/follow/:userId/:userId2", async (req, res) => {
 //Post Detail
 router.get("/article/:postId", async (req, res) => {
   const { postId } = req.params;
-  const post = await user_database.findPostById(postId);
+  const post = await userService.findPostById(postId);
   res.render("post", { post });
 });
 
 //Comment Post
-router.post("/comment/:userId/:postId", async (req, res) => {
-  const { postId, userId } = req.params;
-  const {commentName, commentContent} = req.body
-  const user = await user_database.find(userId);
-  const post = await user_database.findPostById(postId);
-  user.makeCommentById(post, {commentName, commentContent});
-  const result = user_database.updatePostById(post._id, post)
-  console.log(result)
-  res.status(200).send(post);
+router.post("/comment/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const {postId, commentName, commentContent} = req.body
+  
+  const comment = await commentService.makeComment(postId, userId, commentName, commentContent);
+  // const result = userService.updatePostById(post._id, post)
+  console.log("ok")
+  res.send(comment);
 });
 
 //Like Post
 router.post("/like/:userId/:postId", async (req, res) => {
   const { postId, userId } = req.params;
-  const user = await user_database.find(userId);
-  const post = await user_database.findPostById(postId);
+  const user = await userService.find(userId);
+  const post = await userService.findPostById(postId);
   user.likePost(post);
-  await user_database.updatePostById(post._id, post);
+  await userService.updatePostById(post._id, post);
   res.send("ok");
 });
 
 //Dislike Post
 router.post("/dislike/:userId/:postId", async (req, res) => {
   const { postId, userId } = req.params;
-  const user = await user_database.find(userId);
-  const post = await user_database.findPostById(postId);
+  const user = await userService.find(userId);
+  const post = await userService.findPostById(postId);
   user.dislikePost(post);
-  await user_database.updatePostById(post._id, post);
+  await userService.updatePostById(post._id, post);
   res.send("ok");
 });
 
