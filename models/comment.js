3596c37ffac4130ed96bdaddd063f63430e282f8
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-25 23:22:59 +0300

    Comment Schema

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,16 @@
-const uuid = require('uuid')
 const mongoose = require('mongoose')
 
 const CommentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        autopopulate: true
+    },
     commentName: String,
     commentContent: String,
+    
 })
+CommentSchema.plugin(require('mongoose-autopopulate'));
 
 module.exports = mongoose.model("Comment",CommentSchema)
 
