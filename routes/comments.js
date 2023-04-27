22dfa5126e4703bbaf3da6b1934b252715379b9d
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-04-27 23:47:43 +0300

    unnecessary import removed

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {commentService, userService} = require('../services')
+const {commentService} = require('../services')
 
 
 //All comments
