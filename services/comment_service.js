94c5787d43726f03fdf64ec3abf898b3af722298
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-05-23 23:14:45 +0300

    Comment Service

--- services/comment_service.js
+++ services/comment_service.js
@@ -5,15 +5,11 @@ const user_service = require("./user_service");
 class CommentService extends BaseService {
   async makeComment(postId, userId, commentName, commentContent) {
     const user = await user_service.find(userId);
-    const post = await user_service.findPostById(postId);
+    const post = await user_service.findPostById(postId); //Burası düzeltilecek. findPostById kullanılmayacak.
     const comment = await this.insert({user, commentName, commentContent})
     post.comment.push(comment)
     await post.save()
     return comment
-  }
-
-  async findComments(){
-    return this.load()
   }
 }
 
