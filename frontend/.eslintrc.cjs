64f91152ade5a9df84cffaaa0e5d4cccff0b10f4
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-05-30 23:58:54 +0300

    Script setup vue config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,9 +4,8 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-pug/vue3-recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
