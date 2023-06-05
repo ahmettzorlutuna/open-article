4550f8f35cca99d9775f72724d9298be5c9bbe59
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-06-05 23:05:47 +0300

    vue/order-in-components disable

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,9 +4,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
+    'eslint:recommended',
     'plugin:vue/vue3-recommended',
     'plugin:vue-pug/vue3-recommended'
   ],
+  'rules': {
+    'vue/order-in-components': 'off',
+  },
+  "env": {
+    "vue/setup-compiler-macros": true
+  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest'
   }
