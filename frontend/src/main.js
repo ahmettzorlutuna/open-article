d2b4a095de7f8a44171157a12c303e6107d7982d
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-06-05 22:53:34 +0300

    store added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      count: 4
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(store)
 
 app.mount('#app')
