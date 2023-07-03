9e9fdc1a51fd9f1311cadb73ef8aad311a6ff84f
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 23:53:20 +0300

    User detail router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UsersView from '../views/UsersView.vue'
+import UserView from '../views/UserView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: UsersView
+    },
+    {
+      path: '/users/:id',
+      name: 'user',
+      component: UserView
     },
     {
       path: '/about',
