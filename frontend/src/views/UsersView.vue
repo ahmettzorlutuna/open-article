aa32d1887fc990161b06805c74794ba4a332d78c
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 04:20:35 +0300

    Fetch users and listing users on the page

---
+++ frontend/src/views/UsersView.vue
@@ -0,0 +1,43 @@
+<script>
+import { mapActions } from 'vuex';
+
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'UsersView',
+  data() {
+    return {
+      users: [],
+      isLoading: true
+    }
+  },
+  async mounted() {
+    try {
+      this.users = await this.fetchUsers()
+    } catch (e) {
+      this.errMessage = e.message
+    }finally{
+      this.isLoading = false
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    ...mapActions(['fetchUsers'])
+  }
+}
+</script>
+
+<template lang="pug">
+.home
+  HelloWorld(msg="Ana Sayfa")
+  p(v-if="errMessage") {{ errMessage }}
+  p(v-else-if="isLoading") Please wait...
+  div(v-else)
+    p There are {{ users.length }} user in sitee. 
+    ol
+      li(
+        v-for="user in users"
+        :key="user.name")
+        a(:href="`/users/${user._id}`") {{ user.username }} 
+</template>
