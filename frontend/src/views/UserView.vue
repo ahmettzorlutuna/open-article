97fbb573058a7c3eb68683eb41c174f2667d7f0c
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 23:52:42 +0300

    User detail view

---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'UserView',
+    data() {
+        return {
+            isLoading: true,
+            user: {}
+        }
+    }
+}
+</script>
+
+<template lang="pug">
+h1 Welcome User Detail
+.user 
+</template>
