9841fcac0e1b104bebe916db1799fa56a6e11b81
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 04:22:02 +0300

    Base url and fetchUsers() method

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,24 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import { createStore } from 'vuex'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 // Create a new store instance.
 const store = createStore({
   state () {
-    return {
-      count: 4
-    }
+    
   },
   mutations: {
-    increment (state) {
-      state.count++
+    
+  },
+  actions: {
+    async fetchUsers () {
+      const request = await axios.get('/users')
+      return request.data
     }
   }
 })
