6829ac3363acf0198e68ad9a966067091d7b8cdd
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 04:24:45 +0300

    app.use corse

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const bodyParser = require('body-parser')
 const UsersRouter = require('./routes/users')
 const IndexRouter = require('./routes/index')
 const CommentRouter = require('./routes/comments')
+const cors = require('cors')
 require('./mongo-connection')
 
 const app = express()
-const port = 3000
 
 
 app.set('view engine', 'pug')
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/css')); //İnclude css files
+app.use(cors()) //Disable cors policy
+
 app.use('/', IndexRouter)
 app.use('/users', UsersRouter)
 app.use('/comments', CommentRouter)
 
+module.exports = app
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
