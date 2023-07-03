38361b54b552d4fd8ae35ac8c27d42c984faf03d
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-03 04:18:31 +0300

    Hello World components

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,28 +1,26 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+<script>
+
+export default{
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
   }
-})
+}
 </script>
 
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+<template lang="pug">
+.users
+  h1 {{ msg }}
 </template>
 
 <style scoped>
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px;
+  top: -20px;
 }
 
 h3 {
