5273c18874744f8c14b63921699f08a0b6549074
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-07-05 00:39:31 +0300

    users test but disable now

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,19 +1,19 @@
-const app = require('../')
-const request = require('supertest')(app)
+// const app = require('../')
+// const request = require('supertest')(app)
 
-test('create a new user', async() => {
-    const userToCreate = {
-        username: 'cengiz'
-    }
-    const response = await request
-        .post('/users')
-        .send(userToCreate)
-        .expect(200)
+// test('create a new user', async() => {
+//     const userToCreate = {
+//         username: 'cengizzzzz'
+//     }
+//     const response = await request
+//         .post('/users')
+//         .send(userToCreate)
+//         .expect(200)
 
-    const userCreated = response
+//     const userCreated = response
 
-    console.log(userCreated)
+//     console.log(userCreated)
 
-    // expect(userCreated).toMatchObject(userToCreate)
-    // expect(userCreated.bookings).toEqual([])
-})
+//     // expect(userCreated).toMatchObject(userToCreate)
+//     // expect(userCreated.bookings).toEqual([])
+// })
