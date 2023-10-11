3d05e8f25efd05cd070bebf59b55badcac64fccf
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-10-11 23:55:09 +0300

    removing deleted files from tracking

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -4,21 +4,39 @@ export default {
   name: 'AboutView',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      msg: 'Welcome To About View',
+      showHelloWorld: false
+    }
+  },
+  methods: {
+  },
+  computed: {}
 }
 </script>
 
 <template lang="pug">
 .about
-  HelloWorld(msg="About View")
+  HelloWorld(
+    theme="red"
+    )
+    slot
+    template(#links)
+      h1 Register to site
+      a(href="#") Sign in
+      a(href="#") Sign up
+  
+
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
+
+
+
+
+
+
 </style>
