0936e13f95b5af26f9c3a2cc9c86e151d1f33c2e
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-12-11 23:56:08 +0300

    store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from 'vuex'
 
+import axios from 'axios'
+axios.defaults.baseURL = "http://localhost:3000"
+
 export default createStore({
   state: {
   },
@@ -8,6 +11,18 @@ export default createStore({
   mutations: {
   },
   actions: {
+    async fetchUsers () {
+      const request = await axios.get('/users')
+      return request.data
+    },
+    async fetchUser (ctx, passengerId) {
+      const request = await axios.get(`/users/${passengerId}`)
+      return request.data
+    },
+    async deletePost (ctx, postId) {
+      const request = await axios.delete(`/users/posts/${postId}`)
+      return request.data
+    }
   },
   modules: {
   }
