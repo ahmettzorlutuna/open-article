3a76b6ab3e2c78b924899d9dfebf724920504908
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-12-13 23:43:12 +0300

    SingleUser component

---
+++ frontend/src/components/SingleUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user">
+    <a :href="`/users/${user._id}`">{{ user.username }} </a>
+    <p>Followers..:</p>
+    <p>{{ user.followers.length }}</p>
+    <p>Following..:</p>
+    <p>{{ user.following.length }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user"],
+};
+</script>
+
+<style>
+p,a{
+    display: inline;
+    margin: 0 10px 10px 0;
+}
+</style>
