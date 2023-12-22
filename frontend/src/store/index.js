30e9c4838bc456113d5bd0bd31b4a1092e36b85b
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-12-22 23:53:11 +0300

    Global users array, isLoading and error.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,29 +1,47 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
-import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:3000"
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3000";
 
 export default createStore({
   state: {
+    users: [],
+    isLoading: true,
+    errMessage: "",
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
-  },
-  actions: {
-    async fetchUsers () {
-      const request = await axios.get('/users')
-      return request.data
+    setUsers(state, users) {
+      state.users = users;
     },
-    async fetchUser (ctx, passengerId) {
-      const request = await axios.get(`/users/${passengerId}`)
-      return request.data
+    setErrMessage(state,error){
+      state.errMessage = error
     },
-    async deletePost (ctx, postId) {
-      const request = await axios.delete(`/users/posts/${postId}`)
-      return request.data
+    setLoading(state,loading){
+      state.isLoading = loading
     }
   },
-  modules: {
-  }
-})
+  actions: {
+    async fetchUsers({ commit }) {
+      try {
+        commit("setLoading", true);
+        const request = await axios.get("/users");
+        commit("setUsers", request.data);
+        return request.data;
+      } catch (e) {
+        commit("setErrMessage",e)
+      }finally{
+        commit("setLoading",false)
+      }
+    },
+    async fetchUser(ctx, passengerId) {
+      const request = await axios.get(`/users/${passengerId}`);
+      return request.data;
+    },
+    async deletePost(ctx, postId) {
+      const request = await axios.delete(`/users/posts/${postId}`);
+      return request.data;
+    },
+  },
+  modules: {},
+});
