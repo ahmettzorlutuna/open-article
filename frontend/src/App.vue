14f4bd313a23fb0dd6f2a2e29f4bc6fd28bd3040
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2023-12-28 23:53:07 +0300

    some styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,12 +12,9 @@ import NavBar from '@/components/NavBar.vue';
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 *{
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
 }
 </style>
