b6637a723d4a59d34b16e8dfc48c0fc8915de48d
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-02 23:54:27 +0300

    checUserFunc. but ım not use it now

--- services/user_service.js
+++ services/user_service.js
@@ -9,6 +9,12 @@ class UserService extends BaseService{
         await user.save();
         await user2.save();
     }
+    async checkUserAndInsert(key,value,userData){
+        const existingUser = await this.find(key,value)
+        if(!existingUser){
+            await this.insert(userData)
+        }
+    }
 }
 
 module.exports = new UserService(User)
