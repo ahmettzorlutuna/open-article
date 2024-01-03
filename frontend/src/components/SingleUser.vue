9d11c19f6334275d3e8513a29c01b6c87f943928
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-04 00:53:56 +0300

    each user and its post name and content

--- frontend/src/components/SingleUser.vue
+++ frontend/src/components/SingleUser.vue
@@ -1,24 +1,51 @@
 <template>
-  <div class="centered">
-    <a :href="`/users/${user._id}`">{{ user.username }} </a>
-    <p>Followers..:</p>
-    <p>{{ user.followers.length }}</p>
-    <p>Following..:</p>
-    <p>{{ user.following.length }}</p>
+  <div v-for="post in user.posts" class="centered">
+    <div class="cards">
+      <article class="card-content">
+        <h1><a :href="`/users/${user._id}`">{{ user.username }} </a></h1>
+          <h2><a :href="`/users/article/${post._id}`">{{ post.name }}</a></h2>
+          <p>{{ post.content }}</p>
+          <span id="created-date">Created date <span >{{ post.createdDate}}</span></span>
+      </article>
+      <hr class="card-line">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["user"],
+  props: ["user"]
 };
 </script>
 
-<style>
+<style scoped>
 .centered{
-  background-color:cornflowerblue;
   display: flex;
-  justify-content: space-between;
-  flex-direction: column;
+  justify-content: center;
+}
+.cards{
+  --cards-width: 728px;
+  font-family: 'Arial', sans-serif;
+  height: 244px;
+  width: var(--cards-width);
+  position: relative;
+}
+.card-line{
+  width: var(--cards-width);
+  bottom: 0;
+  border: 1px solid rgb(242, 242, 242);
+  position: absolute;
+}
+.card-content a{
+  text-decoration: none;
+  color: black;
+}
+
+#created-date{
+  background-color: rgba(218, 218, 218, 0.600);
+  border-radius: 10px;
+  padding: 5px;
+  text-align: center;
+  margin: 0 5px 0 5px;
 }
 </style>
