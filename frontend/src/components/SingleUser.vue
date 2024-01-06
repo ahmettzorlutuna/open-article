9c119d7dcad7dc0e772675b5456988030233fa61
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-06 23:54:26 +0300

    Short post content

--- frontend/src/components/SingleUser.vue
+++ frontend/src/components/SingleUser.vue
@@ -1,34 +1,38 @@
 <template>
-  <div v-for="post in user.posts" class="centered">
-    <div class="cards">
-      <article class="card-content">
-        <h1><a :href="`/users/${user._id}`">{{ user.username }} </a></h1>
-          <h2><a :href="`/users/article/${post._id}`">{{ post.name }}</a></h2>
-          <p>{{ post.content }}</p>
-          <span id="created-date">Created date <span >{{ post.createdDate}}</span></span>
+      <article v-for="post in user.posts" class="card-container">
+        <h1 id="user-name"><a :href="`/users/${user._id}`">{{ user.username }} </a></h1>
+        <p id="post-name"><a :href="`/users/article/${post._id}`">{{ post.name }}</a></p>
+        <p id="post-content">{{ post.content }}</p>
+        <span id="created-date">Created date <span >{{ post.createdDate}}</span></span>
+        <hr class="card-line">
       </article>
-      <hr class="card-line">
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: ["user"]
+  props: ["user"],
+  computed: {
+    shortenPostContent(){
+      return this.post.content.splice(0,10)
+    }
+  },
+  mounted() {
+    shortenPostContent()
+  },
 };
 </script>
 
 <style scoped>
-.centered{
+.card-container{
   display: flex;
+  flex-flow: column;
   justify-content: center;
-}
-.cards{
   --cards-width: 728px;
-  font-family: 'Arial', sans-serif;
-  height: 244px;
   width: var(--cards-width);
+  min-height: 244px;
+  margin: 5px 20px 40px 20px;
   position: relative;
+  font-family: 'Arial', sans-serif;
 }
 .card-line{
   width: var(--cards-width);
@@ -36,16 +40,21 @@ export default {
   border: 1px solid rgb(242, 242, 242);
   position: absolute;
 }
-.card-content a{
+.card-container a{
   text-decoration: none;
   color: black;
 }
-
-#created-date{
-  background-color: rgba(218, 218, 218, 0.600);
-  border-radius: 10px;
-  padding: 5px;
-  text-align: center;
-  margin: 0 5px 0 5px;
+#user-name{
+  flex-grow: 1;
 }
+#post-name{
+  flex-grow: 1;
+}
+#post-content{
+  flex-grow: 2;
+}
+#created-date{
+  flex-grow: 1;
+}
+
 </style>
