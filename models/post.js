2e8ad3013dbd095068f8c63b1d51058aeb1f9efc
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-07 23:52:56 +0300

    max depth removed for now

--- models/post.js
+++ models/post.js
@@ -14,27 +14,14 @@ const PostSchema = new mongoose.Schema({
   comment: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Comment',
-      autopopulate: {maxDepth: 2},
+      ref: 'Comment'
     },
   ],
   likes: { type: Number, default: 0 },
-  dislikes: { type: Number, default: 0 }
+  dislikes: { type: Number, default: 0 },
+  createdDate: Date
 });
 
 PostSchema.plugin(require('mongoose-autopopulate'));
 
 module.exports = mongoose.model("Post", PostSchema);
-
-// class Post{
-//     constructor(name,content,comment = [], likes = 0, dislikes = 0 , id = uuid.v4()){
-//         this.name = name
-//         this.content = content
-//         this.comment = comment
-//         this.likes = likes
-//         this.dislikes = dislikes
-//         this.postId = id
-//     }
-// }
-
-// module.exports = Post
