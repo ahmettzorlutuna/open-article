f491298c121b632d6d443312f32afa99fb6882df
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-08 23:51:45 +0300

    fethedPost

--- frontend/src/views/PostDetailView.vue
+++ frontend/src/views/PostDetailView.vue
@@ -1,13 +1,49 @@
 <template>
-  
+  <article class="card-container">
+    <!-- <p id="user-name"><a :href="`/users/${user._id}`">{{ post.username }} </a></p> -->
+    <h2 id="post-name">{{ post.name }}</h2>
+    <p id="post-content">{{ post.content }}</p>
+    <span id="created-date">{{ truncateDate }}</span>
+    <hr class="card-line" />
+  </article>
 </template>
 
 <script>
-export default {
+import { mapActions } from "vuex";
 
-}
+export default {
+  data() {
+    return {
+      post: {},
+      isLoading: true,
+      errMessage: "",
+    };
+  },
+  async mounted() {
+    try {
+      await this.updatePost();
+    } catch (error) {
+      this.errMessage = error.message;
+    } finally {
+      this.isLoading = false;
+    }
+  },
+  methods: {
+    ...mapActions(["fetchPost"]),
+    async updatePost() {
+      this.post = await this.fetchPost(this.$route.params.postId);
+    },
+  },
+  computed: {
+    truncateDate() {
+      return parseInt(this.post.createdDate)
+    },
+  },
+};
 </script>
 
 <style>
-
+.card-container {
+  margin-top: 20px;
+}
 </style>
