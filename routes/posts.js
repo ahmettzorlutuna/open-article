b6e8fbb34e9d67f2166d1f019e5c7a6439b606c4
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-08 23:56:37 +0300

    removed postService required

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { userService, postService} = require("../services");
 
 //New post
 router.post("/:userId", async (req, res) => {
@@ -11,23 +12,22 @@ router.post("/:userId", async (req, res) => {
 });
 
 //Delete Post
-router.delete("/posts/:postId", async (req, res) => {
-    const { postId } = req.params.postId;
-    const post = await postService.removePostById(postId);
-    return post;
-  });
-  
-  //Delete User
-  router.delete("/:userId", async (req, res) => {
-    const user = userService.removeById(req.params.userId);
-    res.send(user);
-  });
+router.delete("/:postId", async (req, res) => {
+  const { postId } = req.params.postId;
+  const post = await postService.removePostById(postId);
+  return post;
+});
+
+//Delete User
+router.delete("/:userId", async (req, res) => {
+  const user = userService.removeById(req.params.userId);
+  res.send(user);
+});
 
 //Post Detail
-router.get("/article/:postId", async (req, res) => {
-  const { postId } = req.params;
-  const post = await postService.findByObjectId(postId);
-  res.render("post", { post });
+router.get("/:postId", async (req, res) => {
+  const post = await postService.find(req.params.postId);
+  res.send(post);
 });
 
 //Like Post
