a5e23050bd183f6e27b3ba2708f243d40c64a5e3
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-15 23:55:13 +0300

    config new mayve

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+}
