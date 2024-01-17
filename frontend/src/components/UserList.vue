ce4bcaa727d603e5b725e6bd08a3f600983746df
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-01-17 23:54:03 +0300

    imported with new name SinglePost

--- frontend/src/components/UserList.vue
+++ frontend/src/components/UserList.vue
@@ -3,7 +3,6 @@
     <p v-if="errMessage">{{ errMessage }}</p>
     <p v-else-if="isLoading">Please wait...</p>
     <div v-else>
-      <!-- <p>There are {{ users.length }} user in sitee.</p> -->
       <ol>
         <div v-for="user in users" :key="user.name">
           <SingleUser :user="user" />
@@ -13,7 +12,7 @@
   </div>
 </template>
 <script>
-import SingleUser from "./SingleUser.vue";
+import SingleUser from "./SinglePost.vue";
 export default {
   props: ["users", "isLoading", "errMessage"],
   components: { SingleUser },
