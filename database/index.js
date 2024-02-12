241ab3795842d0fe3c33c4e897fbe1c66c555169
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-02-12 20:41:35 +0300

    Database folder is no longer used

--- database/index.js
+++
@@ -1,3 +0,0 @@
-const userDatabase = require('./user_database')
-
-module.exports = userDatabase
--- database/user_database.js
+++
@@ -1,8 +0,0 @@
-const BaseDatabase = require('./base_database')
-const User = require('../models/user') 
-
-class UserDatabase extends BaseDatabase{
-
-}
-
-module.exports = new UserDatabase(User)
