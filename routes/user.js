d7c9b09dc8c8c90ec96c5f6224a8141b982c645e
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-02-29 23:46:20 +0300

    express  login and register routes

---
+++ routes/user.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const utils = require("../auth/utils");
+const User = require("../models/user");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const { userService } = require("../services");
+
+router.get("/protected", passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.status(200).json({success: true, msg: "You are authorized !"})
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (!user)
+      res.status(401).json({ success: false, msg: "Could not find the user!" });
+
+    const isValid = utils.validatePassword(
+      req.body.password,
+      user.hash,
+      user.salt
+    );
+
+    if (isValid) {
+      //We are not issued the jwt with user yet.
+      const tokenObject = utils.issueJWT(user);
+
+      res.status(200).json({ success: true, user: user, token: tokenObject });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, msg: "You entered the wrong password" });
+    }
+  })
+  .catch((err) => {
+    next(err)
+  });
+});
+
+router.get("/register", (req, re, next) => {});
+
+router.post("/register", (req, res, next) => {
+  const saltHash = utils.genPassword(req.body.password);
+
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+    hash: hash,
+    salt: salt,
+  });
+
+  newUser
+    .save()
+    .then((user) => {
+      const jwt = utils.issueJWT(user);
+
+      res.json({
+        success: true,
+        user: user,
+        token: jwt.token,
+        expiresIn: jwt.expires,
+      });
+    })
+    .catch((err) => next(err));
+});
+module.exports = router;
