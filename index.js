579cf3743d5e32921349da1f2a665749c05cef3f
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-02-29 23:41:11 +0300

    some passport configuration

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const UsersRouter = require("./routes/users");
 const IndexRouter = require("./routes/index");
 const CommentRouter = require("./routes/comments");
 const PostRouter = require("./routes/posts");
+const UserRouter = require("./routes/user");
 
-const passport = require("passport-jwt");
+const passport = require("passport");
 const cors = require("cors");
 const path = require("path"); //Accessing file system
 
 require("./mongo-connection");
 
+//Passed global passport library object into the configuration function
+require("./config/passport")(passport);
+
 const app = express();
 
-app.set("view engine", "pug");
+//İnitialized the passport js object on every request
+app.use(passport.initialize());
 
+// app.use(express.json());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "/frontend/public")));
 
 app.use(express.static(__dirname + "/css")); //İnclude css files
 
@@ -26,5 +34,6 @@ app.use("/", IndexRouter);
 app.use("/users", UsersRouter);
 app.use("/comments", CommentRouter);
 app.use("/posts", PostRouter);
+app.use("/user", UserRouter);
 
 module.exports = app;
