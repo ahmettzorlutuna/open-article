5b04b21e8cb4d6143fb18e9c60a455a137a9b067
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-02-29 23:44:44 +0300

    Register view form

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,5 +1,15 @@
 <template>
   <h1>Register Page</h1>
+  <form>
+        <h2>Signup to app</h2>
+        <label for="email">Email</label>
+        <input type="text" name="email" required>
+        <div class="email-error"></div>
+        <label for="password">Password</label>
+        <input type="password" name="password" required>
+        <div class="password-error"></div>
+        <button>Sign up</button>
+    </form>
 </template>
 
 <script>
