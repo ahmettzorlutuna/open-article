b57bbe415212b1ea150d318849423b9219639ac4
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-03-03 23:56:58 +0300

    useless scripts removed

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,19 +4,6 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
-import { useStore } from "vuex";
-import PostsList from "@/components/PostsList.vue";
-
-const store = useStore();
-
-const users = computed(() => store.state.users);
-const errMessage = computed(() => store.state.errMessage);
-const isLoading = computed(() => store.state.isLoading);
-
-onMounted(() => {
-  store.dispatch('fetchUsers')
-})
 </script>
 
 <style scoped>
