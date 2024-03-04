b9b9911def6433f10e010f91a6b459cae09007cc
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-03-04 22:52:11 +0300

    using username instead email

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,9 +2,9 @@
   <h1>Login page</h1>
   <form>
         <h2>Login to app</h2>
-        <label for="email">Email</label>
-        <input type="text" name="email" required>
-        <div class="email-error"></div>
+        <label for="email">Username</label>
+        <input type="text" name="username" required>
+        <div class="username-error"></div>
         <label for="password">Password</label>
         <input type="password" name="password" required>
         <div class="password-error"></div>
