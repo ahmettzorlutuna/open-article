976aec6a20f7e7389733ee9c209a82f36e873510
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-03-05 23:53:43 +0300

    Login view axios post request

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,21 +1,40 @@
 <template>
-  <h1>Login page</h1>
-  <form>
-        <h2>Login to app</h2>
-        <label for="email">Username</label>
-        <input type="text" name="username" required>
-        <div class="username-error"></div>
-        <label for="password">Password</label>
-        <input type="password" name="password" required>
-        <div class="password-error"></div>
-        <button>Sign up</button>
-    </form>
+  <h1>Login Page</h1>
+  <form @submit.prevent="submitForm()">
+    <h2>Login to app</h2>
+    <label for="username">Username:</label>
+    <input type="text" required v-model="username" />
+    <label for="password">Password:</label>
+    <input type="text" required v-model="password" />
+    <button type="submit">Submit</button>
+  </form>
 </template>
 
-<script>
-export default {
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+const authService = require("../services/AuthService")
 
-}
+const username = ref("");
+const password = ref("");
+
+const submitForm = async () => {
+  try {
+    const reqObject = {
+      username: username.value,
+      password: password.value,
+    };
+
+    const response = await axios.post("http://localhost:3000/user/login", reqObject);
+
+    authService.setLocalStorage(response);
+
+    console.log(response);
+  } catch (error) {
+    // Handle errors here
+    console.error("Error:", error);
+  }
+};
 </script>
 
 <style>
