053bf495d9234384f63ce24f2e609da81e4a1d29
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-03-09 23:49:27 +0300

    auth strategy

--- auth/passport.js
+++ auth/passport.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const User = require("../models/user");
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const pathToKey = path.join(__dirname, "../auth", "pub_key.pem");
 const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
@@ -14,20 +14,24 @@ const options = {
   algorithms: ["RS256"],
 };
 
-const strategy = new JwtStrategy(options, (jwt_payload, done) => {
-  User.findOne({id: jwt_payload.sub}, function(err, user) {
-    if (err) {
-        return done(err, false);
-    }
-    if (user) {
-        return done(null, user);
-    } else {
-        return done(null, false);
-        // or you could create a new account
-    }
-});
-});
 
 module.exports = (passport) => {
-  passport.use(strategy); 
-};
+  // The JWT payload is passed i    nto the verify callback
+  passport.use(new JwtStrategy(options, function(jwt_payload, done) {
+      
+      // We will assign the `sub` property on the JWT to the database ID of user
+      User.findOne({_id: jwt_payload.sub}, function(err, user) {
+          
+          if (err) {
+              return done(err, false);
+          }
+          if (user) {
+              return done(null, user);
+          } else {  
+              return done(null, false);
+          }
+          
+      });
+      
+  }));
+}
