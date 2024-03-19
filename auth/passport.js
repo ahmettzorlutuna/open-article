e7ee4a35b9ee48cbd8d1366c1e7c161056e3fe22
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2024-03-19 23:46:40 +0300

    moved to the index.js

--- auth/passport.js
+++ auth/passport.js
@@ -1,37 +1,35 @@
-const path = require("path");
-const fs = require("fs");
-const User = require("../models/user");
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
+// const path = require("path");
+// const fs = require("fs");
+// const User = require("../models/user");
+// var JwtStrategy = require("passport-jwt").Strategy,
+//   ExtractJwt = require("passport-jwt").ExtractJwt;
 
-const pathToKey = path.join(__dirname, "../auth", "pub_key.pem");
-const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
+// const pathToKey = path.join(__dirname, "../auth", "pub_key.pem");
+// const PUB_KEY = fs.readFileSync(pathToKey, "utf8");
 
-const options = {
-  //Passport verification process
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: PUB_KEY,
-  algorithms: ["RS256"],
-};
+// const options = {
+//   //Passport verification process
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//   secretOrKey: PUB_KEY,
+//   algorithms: ["RS256"],
+// };
 
-
-module.exports = (passport) => {
-  // The JWT payload is passed i    nto the verify callback
-  passport.use(new JwtStrategy(options, function(jwt_payload, done) {
-      
-      // We will assign the `sub` property on the JWT to the database ID of user
-      User.findOne({_id: jwt_payload.sub}, function(err, user) {
-          
-          if (err) {
-              return done(err, false);
-          }
-          if (user) {
-              return done(null, user);
-          } else {  
-              return done(null, false);
-          }
-          
-      });
-      
-  }));
-}
+// module.exports = (passport) => {
+//   // The JWT payload is passed into the verify callback
+//   passport.use(
+//     new JwtStrategy(options, function (jwt_payload, done) {
+//         console.log("payload recieved",jwt_payload)
+//       // We will assign the `sub` property on the JWT to the database ID of user
+//       User.findOne({ _id: jwt_payload.sub }, function (err, user) {
+//         if (err) {
+//           return done(err, false);
+//         }
+//         if (user) {
+//           return done(null, user);
+//         } else {
+//           return done(null, false);
+//         }
+//       });
+//     })
+//   );
+// };
